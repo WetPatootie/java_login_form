49e03a71c268a46f1a3113414cfee46bd3436ccd
Wet Patootie <jerardmichaelaquino5@gmail.com>
2018-01-09 17:29:16 +0800

    reee

---
+++ Gui/src/com/patootie/Main.java
@@ -0,0 +1,32 @@
+package com.patootie;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    Button button;
+
+    public static void main(String[] args){
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("Application");
+        button = new Button();
+        button.setText("Click Me!");
+
+        //set button on the middle of the screen
+        StackPane layout = new StackPane();
+        layout.getChildren().add(button);
+
+        Scene scene = new Scene(layout, 800, 600);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+}
